10b5658cca34e87eaf90894980465752c1cbf4d8
Akash Raj <117104497+akashraj0901@users.noreply.github.com>
2026-04-21 11:45:23 +0530

    Update and rename p3.sql to Key_Business_Problems.sql

--- Key_Business_Problems.sql
+++ Key_Business_Problems.sql
@@ -11,34 +11,33 @@ where sale_date = '2022-11-05';
 select * from retail_sales
 where category = 'Clothing'
 and quantiy > 4
-and sale_date between '2022-11-01' and '2022-11-30'; 		-- Data format in MySQL "YYYY-MM-DD"
-
-select * from retail_sales
-where category = 'Clothing'
-and quantiy > 4
 and sale_date >= '2022-11-01'
-and sale_date < '2022-12-01';				-- this code is more ideal beacuse in 'Between' it may miss some rows..
+and sale_date < '2022-12-01';				
 
 
 
 -- 3. Write a SQL query to calculate the total sales (total_sale) for each category.:
-select category, sum(total_sale) as net_total, COUNT(*) from retail_sales
+select category, sum(total_sale) as net_total, COUNT(*) 
+    from retail_sales
 group by category;
 
 -- 4. Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.:
-select category , round(avg(age),2) as avg_age from retail_sales
-group by category having category = 'Beauty'; 
-
-select round(avg(age),2) as avg_age from retail_sales
-where category = 'Beauty';									-- More Fast and cleaner
+select round(avg(age),2) as avg_age 
+    from retail_sales
+where 
+    category = 'Beauty';									
 
 -- 5.Write a SQL query to find all transactions where the total_sale is greater than 1000.:
 select * from retail_sales
-where total_sale > 1000 order by total_sale desc;
+where 
+    total_sale > 1000 order by total_sale desc;
 
 -- 6. Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.:
-select category, gender, count(transactions_id) as total_transaction from retail_sales
-group by category, gender order by category asc;
+select 
+    category, gender, count(transactions_id) as total_transaction 
+    from retail_sales
+group by 
+    category, gender order by category asc;
 
 -- 7. Write a SQL query to calculate the average sale for each month. Find out best selling month in each year:
 SELECT 
@@ -65,8 +64,10 @@ WHERE rnk = 1;
 
 
 -- 8. **Write a SQL query to find the top 5 customers based on the highest total sales **:
-select customer_id, sum(total_sale) as net_sales from retail_sales
-group by customer_id order by net_sales desc limit 5;
+select customer_id, sum(total_sale) as net_sales 
+    from retail_sales
+group by 
+    customer_id order by net_sales desc limit 5;
 
 -- 9. Write a SQL query to find the number of unique customers who purchased items from each category.:
 select category, count(distinct customer_id) as unique_customer from retail_sales

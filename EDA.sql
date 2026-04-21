1b7e22d92b0157e1711ad51a152b19660cb928e9
Akash Raj <117104497+akashraj0901@users.noreply.github.com>
2026-04-21 11:41:27 +0530

    Update and rename p2.sql to EDA.sql

--- EDA.sql
+++ EDA.sql
@@ -6,8 +6,7 @@ use project_1;
 select count(*) from retail_sales;
 
 -- How many customer we have
-select count(customer_id) from retail_sales;    -- it is worng because one customer might have bought more then once XXXX
-select count(distinct customer_id) from retail_sales;  -- It will give exact no. of customers
+select count(distinct customer_id) from retail_sales;  
 
 -- How many catogries we have
 select count(distinct category) from retail_sales;
